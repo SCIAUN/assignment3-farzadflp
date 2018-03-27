45d848aef1d5eb73aab56d06abea5cd63e0e9ded
Farzad Falahpour <farzad@farzad.local>
2018-03-27 17:13:00 +0430

    run
    p30download.com 	 21
    timed out
    
    p30download.com 	 22
    timed out
    
    p30download.com 	 25
    timed out
    
    p30download.com 	 80
    timed out
    
    p30download.com 	 110
    timed out
    
    p30download.com 	 9090
    timed out
    
    google.com 	 21
    timed out
    
    google.com 	 22
    timed out
    
    google.com 	 25
    timed out
    
    google.com 	 80
    timed out
    
    google.com 	 110
    timed out
    
    google.com 	 9090
    timed out
    
    downloadha.com 	 21
    [-] FTP server is not vulnerable
    
    downloadha.com 	 22
    [Errno 61] Connection refused
    
    downloadha.com 	 25
    timed out
    
    downloadha.com 	 80
    timed out
    
    downloadha.com 	 110
    [-] FTP server is not vulnerable
    
    downloadha.com 	 9090
    timed out
    
    iaun.ac.ir 	 21
    [Errno 61] Connection refused
    
    iaun.ac.ir 	 22
    [Errno 61] Connection refused
    
    iaun.ac.ir 	 25
    timed out
    
    iaun.ac.ir 	 80
    timed out
    
    iaun.ac.ir 	 110
    [Errno 61] Connection refused
    
    iaun.ac.ir 	 9090
    timed out
    
    maktabkhooneh.org 	 21
    [Errno 61] Connection refused
    
    maktabkhooneh.org 	 22
    [-] FTP server is not vulnerable
    
    maktabkhooneh.org 	 25
    [-] FTP server is not vulnerable
    
    maktabkhooneh.org 	 80
    timed out
    
    maktabkhooneh.org 	 110
    [Errno 61] Connection refused
    
    maktabkhooneh.org 	 9090
    [Errno 61] Connection refused
    
    soft98.ir 	 21
    [-] FTP server is not vulnerable
    
    soft98.ir 	 22
    [Errno 61] Connection refused
    
    soft98.ir 	 25
    timed out
    
    soft98.ir 	 80
    timed out
    
    soft98.ir 	 110
    [-] FTP server is not vulnerable
    
    soft98.ir 	 9090
    timed out
    
    Process finished with exit code 0

--- assignment3.py
+++ assignment3.py
@@ -1,14 +1,30 @@
 import socket
 
-ports=[21,22,25,80,110,9090]
-socket.setdefaulttimeout(5)
-f=open('host_names','r')
-host_names=f.read()
-host_names=host_names.split('\n')
+ports=[21,22,25,80,110,9090];
+socket.setdefaulttimeout(5);
+f=open('host_names','r');
+host_names=f.read();
+host_names=host_names.split('\n');
 
 for x in host_names:
     for y in ports:
         try:
            # you should write your codes in this block
+
+            print(x,'\t',y);
+            s=socket.socket();
+            s.connect((x,y));
+            ans=s.recv(y);
+            ans=str(ans);
+            if "FreeFloat Ftp Server (Version 1.00)" in ans:
+                print("[+] FreeFloat server is vulnerable ",'\n');
+            elif "3Com 3CDaemon FTP Server Version 2.0" in ans:
+                print("[+] 3CDaemon server is vulnerable ",'\n');
+            elif "Ability Server 2.34" in ans:
+                print("[+] Ability server is vulnerable ",'\n');
+            elif "Sami FTP Server 2.0.2" in ans:
+                print("[+] Sami server is vulnerable ",'\n');
+            else:
+                print("[-] FTP server is not vulnerable",'\n');
         except Exception as e:
-            print(e)
+            print(e,'\n');
